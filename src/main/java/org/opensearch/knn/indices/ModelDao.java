9cade2aab87c07cd2a2c5ea9d06105b1aefc6cba
John Mazanec <jmazane@amazon.com>
2021-06-30 10:39:36 -0700

    Add dimension to model persistence
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- src/main/java/org/opensearch/knn/indices/ModelDao.java
+++ src/main/java/org/opensearch/knn/indices/ModelDao.java
@@ -176,6 +176,7 @@ public interface ModelDao {
             Map<String, Object> parameters = ImmutableMap.of(
                     KNNConstants.KNN_ENGINE, model.getKnnEngine().getName(),
                     KNNConstants.METHOD_PARAMETER_SPACE_TYPE, model.getSpaceType().getValue(),
+                    KNNConstants.DIMENSION, model.getDimension(),
                     KNNConstants.MODEL_BLOB_PARAMETER, base64Model
             );
 
@@ -193,6 +194,7 @@ public interface ModelDao {
             Map<String, Object> parameters = ImmutableMap.of(
                     KNNConstants.KNN_ENGINE, model.getKnnEngine().getName(),
                     KNNConstants.METHOD_PARAMETER_SPACE_TYPE, model.getSpaceType().getValue(),
+                    KNNConstants.DIMENSION, model.getDimension(),
                     KNNConstants.MODEL_BLOB_PARAMETER, base64Model
             );
 
@@ -220,7 +222,7 @@ public interface ModelDao {
             }
 
             /*
-                GET /<model_index>/<modelId>?source_includes=<model_blob>&_local
+                GET /<model_index>/<modelId>?_local
             */
             GetRequestBuilder getRequestBuilder = new GetRequestBuilder(client, GetAction.INSTANCE, MODEL_INDEX_NAME)
                     .setId(modelId)
@@ -230,6 +232,7 @@ public interface ModelDao {
             Map<String, Object> responseMap = getResponse.getSourceAsMap();
             Object engine = responseMap.get(KNNConstants.KNN_ENGINE);
             Object space = responseMap.get(KNNConstants.METHOD_PARAMETER_SPACE_TYPE);
+            Object dimension = responseMap.get(KNNConstants.DIMENSION);
             Object blob = responseMap.get(KNNConstants.MODEL_BLOB_PARAMETER);
 
             if (blob == null) {
@@ -238,7 +241,7 @@ public interface ModelDao {
             }
 
             return new Model(KNNEngine.getEngine((String) engine), SpaceType.getSpace((String) space),
-                    Base64.getDecoder().decode((String) blob));
+                    (Integer) dimension, Base64.getDecoder().decode((String) blob));
         }
 
         private String getMapping() throws IOException {
